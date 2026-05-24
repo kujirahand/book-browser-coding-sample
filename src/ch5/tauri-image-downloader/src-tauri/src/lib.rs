514e726d51f9475cddbbbc3bf2e56d0b42df7e87
kujirahand <web@kujirahand.com>
2026-05-24 22:32:40 +0900

    fix typo

--- src/ch5/tauri-image-downloader/src-tauri/src/lib.rs
+++ src/ch5/tauri-image-downloader/src-tauri/src/lib.rs
@@ -9,7 +9,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             html_query::collect_images, // 画像URLを収集する関数 --- (*3)
-            downloader::download // 画像URLをダウンロードする関数)
+            downloader::download // 画像URLをダウンロードする関数
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
